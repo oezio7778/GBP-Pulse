13f85d6883fa7e36b928e712a5d9c8a226db19fa
oezio7778 <os7778@gmail.com>
2025-12-04 17:25:37 -0500

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,9 @@
+// vite-env.d.ts
+
+// This declaration is critical for fixing TS2591 ("Cannot find name 'process'") during Vercel builds.
+declare const process: any;
+
+/// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
