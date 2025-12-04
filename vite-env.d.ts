2b9e8cdfbd7ba8b38e953be4d1c161440e87f83f
oezio7778 <os7778@gmail.com>
2025-12-03 21:39:11 -0500

    Fix type definition error by removing vite/client reference
    Removed vite/client reference to resolve type definition error.

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,17 +1,8 @@
-// Reference to vite/client removed to fix "Cannot find type definition file" error
-// /// <reference types="vite/client" />
-
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
+  [key: string]: any;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-}
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    API_KEY: string;
-    [key: string]: string | undefined;
-  }
 }
